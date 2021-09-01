60fd3e49b65f807a1d2acb5de04ad9ca3bd5fb51
John Fitzpatrick <johnfitzpatrick@Johns-MacBook-Pro-2.local>
2021-09-01 13:30:55 +0100

    Updated troubleshooting section

---
+++ troubleshooting/create-broken-lab1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~/kong-gateway-operations/troubleshooting
+DECKFILE="/home/labuser/kong-gateway-operations/installation/deck/deck.yaml"
+sed -i "s|KONG_ADMIN_API_URI|$KONG_ADMIN_API_URI|g" $DECKFILE > /dev/null 2>&1
+deck dump --config $DECKFILE 
+deck sync --config $DECKFILE -s Broken-Exam-lab-GW-Asso05.yaml > /dev/null 2>&1
+ 
