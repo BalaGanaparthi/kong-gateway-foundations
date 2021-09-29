9618c977396819921b6f483cb7c67c3168767e4b
John Fitzpatrick <johnfitzpatrick@Johns-MacBook-Pro-2.local>
2021-09-29 15:52:51 +0100

    Minor updtes

--- troubleshooting/create-broken-lab1.sh
+++ troubleshooting/create-broken-lab1.sh
@@ -4,5 +4,5 @@ cd ~/kong-gateway-operations/troubleshooting
 DECKFILE="/home/labuser/kong-gateway-operations/installation/deck/deck.yaml"
 sed -i "s|KONG_ADMIN_API_URI|$KONG_ADMIN_API_URI|g" $DECKFILE > /dev/null 2>&1
 deck dump --config $DECKFILE
-yes|deck reset --config $DECKFILE
+yes | deck reset --config $DECKFILE
 deck sync --config $DECKFILE -s kong-brokenlab1.yaml > /dev/null 2>&1
