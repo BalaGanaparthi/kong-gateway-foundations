b53f92e66d225bba0d281f8c5295c9bde0c55e8a
Kimball Johnson <kimball.johnson@konghq.com>
2021-10-05 15:35:06 +0100

    Rename broken-lab2
    Signed-off-by: Kimball Johnson <kimball.johnson@konghq.com>

--- troubleshooting/create-broken-lab2.sh
+++ troubleshooting/create-broken-lab2.sh
@@ -4,4 +4,4 @@ cd ~/kong-gateway-operations/troubleshooting
 DECKFILE="/home/labuser/kong-gateway-operations/installation/deck/deck.yaml"
 sed -i "s|KONG_ADMIN_API_URI|$KONG_ADMIN_API_URI|g" $DECKFILE > /dev/null 2>&1
 deck dump --config $DECKFILE 
-deck sync --config $DECKFILE -s Broken-Exam-lab-GW-Asso05.yaml > /dev/null 2>&1
+deck sync --config $DECKFILE -s broken-lab2.yaml > /dev/null 2>&1
