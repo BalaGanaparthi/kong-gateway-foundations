e9098f630a14891acd01cfd0fd566d7a2dc88608
rexcatus <arash.eftekhari@konghq.com>
2022-03-23 09:57:34 -0700

    Added start script

---
+++ installation/getgo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+cd ~/
+./setup-docker.sh
+git clone https://github.com/kong-education/kong-gateway-operations.git
+cd kong-gateway-operations/installation
+cp -R ssl-certs /srv/shared
+mkdir -p /srv/shared/logs
+touch $(grep '/srv/shared/logs/' docker-compose.yaml|awk '{print $2}'|xargs)
+chmod a+w /srv/shared/logs/*
+docker-compose up -d
+http POST "kongcluster:8001/licenses" payload=@/etc/kong/license.json
+docker-compose stop kong-cp; docker-compose rm -f kong-cp; docker-compose up -d kong-cp
+echo $KONG_MANAGER_URI
+bash sleep 6
+http --headers GET kongcluster:8001
