009fc8f2c445da297b8eedc9762b174676d978cf
rexcatus <arash.eftekhari@konghq.com>
2022-03-23 10:11:33 -0700

    Update

--- installation/getgo.sh
+++ installation/getgo.sh
@@ -9,8 +9,9 @@ mkdir -p /srv/shared/logs
 touch $(grep '/srv/shared/logs/' docker-compose.yaml|awk '{print $2}'|xargs)
 chmod a+w /srv/shared/logs/*
 docker-compose up -d
+sleep 8
 http POST "kongcluster:8001/licenses" payload=@/etc/kong/license.json
 docker-compose stop kong-cp; docker-compose rm -f kong-cp; docker-compose up -d kong-cp
 echo $KONG_MANAGER_URI
-bash sleep 6
+sleep 8
 http --headers GET kongcluster:8001
