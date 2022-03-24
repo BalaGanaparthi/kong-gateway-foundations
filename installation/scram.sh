eed518445e26d1a2384edd309fa698a098a621bf
rexcatus <arash.eftekhari@konghq.com>
2022-03-23 17:05:03 -0700

    Updated

--- installation/scram.sh
+++ installation/scram.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-
 cd ~/
 ./setup-docker.sh
 git clone https://github.com/gigaprimatus/kong-gateway-operations.git
@@ -14,18 +13,5 @@ http POST "kongcluster:8001/licenses" payload=@/etc/kong/license.json
 docker-compose stop kong-cp; docker-compose rm -f kong-cp; docker-compose up -d kong-cp
 echo $KONG_MANAGER_URI
 sleep 8
-http --headers GET kongcluster:8001
-
-
-
-./setup-docker.sh
-git clone https://github.com/gigaprimatus/kong-gateway-operations.git
-cd kong-gateway-operations/installation
-cp -R ssl-certs /srv/shared
-mkdir -p /srv/shared/logs
-touch $(grep '/srv/shared/logs/' docker-compose.yaml|awk '{print $2}'|xargs)
-chmod a+w /srv/shared/logs/*
-docker-compose up -d
-sleep 8
-http --headers GET kongcluster:8001
-http POST "kongcluster:8001/licenses" payload=@/etc/kong/license.json
+http --headers GET kongcluster:8001 | jq .version
+env | grep KONG | sort
