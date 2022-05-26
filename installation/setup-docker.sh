a19ccc5a05e121aa5a3e8e33630819dcc1a09033
rexcatus <arash.eftekhari@konghq.com>
2022-05-26 12:09:20 -0700

    Updated early deploy scripts

--- installation/setup-docker.sh
+++ installation/setup-docker.sh
@@ -5,8 +5,9 @@ normal=$(tput sgr0)
 
 printf "\n${red}Setting up CA, certificate and key for Docker.${normal}"
 mkdir -p ~/.docker
+cp misc/docker-config.json ~/.docker/config.json
 curl -so ~/.docker/ca.pem http://docker:9000/ca.pem
 curl -so ~/.docker/cert.pem http://docker:9000/cert.pem
 curl -so ~/.docker/key.pem http://docker:9000/key.pem
 docker-compose pull &>/dev/null &
-printf "\n${red}Docker Setup complete.${normal}"
+printf "\n${red}Docker Setup complete.${normal}\n\n"
