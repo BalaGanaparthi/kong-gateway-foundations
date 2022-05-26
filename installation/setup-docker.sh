8720bb49d4309aa6d2a752a05427cd198a1fe9a4
rexcatus <arash.eftekhari@konghq.com>
2022-05-26 12:21:02 -0700

    Updated early pull script

--- installation/setup-docker.sh
+++ installation/setup-docker.sh
@@ -9,5 +9,10 @@ cp misc/docker-config.json ~/.docker/config.json
 curl -so ~/.docker/ca.pem http://docker:9000/ca.pem
 curl -so ~/.docker/cert.pem http://docker:9000/cert.pem
 curl -so ~/.docker/key.pem http://docker:9000/key.pem
+mkdir -p /srv/shared/misc
+cp loopback.yaml /srv/shared/misc
+cp misc/kong_realm_template.json /srv/shared/misc
+cp misc/prometheus.yaml /srv/shared/misc
+cp misc/statsd.rules.yaml /srv/shared/misc
 docker-compose pull &>/dev/null &
 printf "\n${red}Docker Setup complete.${normal}\n\n"
